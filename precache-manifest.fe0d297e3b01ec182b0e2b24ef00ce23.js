861947acaadd2bbc7969bc133b3a860398ed724a
Jim Buell <jamesgbuell@gmail.com>
2018-10-23 12:43:52 -0500

    Updates

---
+++ precache-manifest.fe0d297e3b01ec182b0e2b24ef00ce23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "74223d5e5f89bb3a5b1a",
+    "url": "/contactmanager/static/js/main.74223d5e.chunk.js"
+  },
+  {
+    "revision": "a8378dd5e66cbf95254e",
+    "url": "/contactmanager/static/js/1.a8378dd5.chunk.js"
+  },
+  {
+    "revision": "74223d5e5f89bb3a5b1a",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "a8378dd5e66cbf95254e",
+    "url": "/contactmanager/static/css/1.71cfd1fa.chunk.css"
+  },
+  {
+    "revision": "ac19f84645b766eeb7415b2d2d9cb873",
+    "url": "/contactmanager/index.html"
+  }
+];
